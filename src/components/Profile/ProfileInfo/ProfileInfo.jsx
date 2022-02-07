da21553ae666874ea130b9992dc62c638d5644ec
Aidanwalle <dionis.serdiukov@yandex.ru>
2022-02-07 19:25:00 +0200

    lesson 60

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
+import Preloader from "../../Common/Preloader/Preloader";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader/>
+    }
+
     return (
         <div>
             <div>
@@ -9,8 +14,11 @@ const ProfileInfo = () => {
                 {/*    src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' alt='изображение'/>*/}
             </div>
             <div className={s.descriptionBlock}>
-                <img src='https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg' alt='аватар'/>
-                <div>Дионис Сердюков</div>
+                {/*<img src='https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg' alt='аватар'/>*/}
+                <img src={props.profile.photos.large}/>
+                <div>{`Имя пользователя: ${props.profile.fullName}`}</div>
+                <div>{props.profile.contacts.github}</div>
+                <div>{props.profile.contacts.youtube}</div>
             </div>
         </div>
     )
